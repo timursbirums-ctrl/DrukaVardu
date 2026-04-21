298ac9ef8f3f5fc8e5e21daafa34693cf3b364d1
timursbirums-ctrl <timursbirums@gmail.com>
2026-04-21 08:39:08 +0300

    Add files via upload

---
+++ 21.04.py
@@ -0,0 +1,13 @@
+import json
+valstis=[]
+lauksaimniecubas_ipatsvars=[]
+with open("uzd1.json",encoding="utf-8") as datne:
+
+for vardnica in dati:
+    if vardnica ["nosaukums"]:
+    print(["nosaukums"])
+
+print(f'viss aizņem: '["mezs"]+["lauksaimniecibas_zeme"]+["cita_veida_zeme"])
+
+jauna_vardnica=valstis+lauksaimniecubas_ipatsvars
+as open("rezultati.csv","\t",encoding="utf-8")
